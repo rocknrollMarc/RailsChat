66fbc8569855f6b405e5414542c0780e0df75bff
Marc Bluemner <marc.bluemner@gmail.com>
2016-08-24 20:29:10 +0200

    mount action cable

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+	mount ActionCable.server => '/cable'
   devise_for :users
 	resources :chat_rooms, only: [:new, :create, :show, :index]
 
 	root 'chat#index'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
