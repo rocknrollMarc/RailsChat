9b9760df47c1d7493dec3a7b71a157e7d8e937a3
Marc Bluemner <marc.bluemner@gmail.com>
2016-08-24 22:25:24 +0200

    fix typo in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-	mount ActionCable.server => '/cable'
   devise_for :users
+	mount ActionCable.server => '/cable'
 	resources :chat_rooms, only: [:new, :create, :show, :index]
 
-	root 'chat#index'
+	root 'chat_rooms#index'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
